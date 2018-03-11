feafb04533770cf51e1a11a395b68d9de9d98d07
Cassiano <cassianodoneda@gmail.com>
2018-03-11 13:39:37 -0300

    List Evaluation template
    Signed-off-by: Cassiano <cassianodoneda@gmail.com>

---
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/EvaluationController.java
@@ -0,0 +1,20 @@
+package br.edu.ifrs.canoas.tads.tcc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by cassiano on 3/11/18.
+ */
+@Controller
+@RequestMapping("/evaluation")
+public class EvaluationController {
+
+	@GetMapping("/list")
+	public ModelAndView greetings() {
+		return new ModelAndView("/evaluation/list");
+	}
+
+}

753ac5b33c6ec069447e4cb5891f1abcd05073fb
mariele <marielefontana@live.com>
2018-03-20 00:39:07 -0300

    Create model, repository and service to Schedule

--- src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/ScheduleController.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/ScheduleController.java
@@ -3,13 +3,24 @@ package br.edu.ifrs.canoas.tads.tcc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifrs.canoas.tads.tcc.service.ScheduleService;
 
 @RequestMapping("/schedule")
 @Controller
 public class ScheduleController {
+	
+	private final ScheduleService scheduleService;
+
+	public ScheduleController(ScheduleService scheduleService) {
+		super();
+		this.scheduleService = scheduleService;
+	}
 
 	@GetMapping("/index")
+    @RequestMapping(method = RequestMethod.GET)
 	public ModelAndView home() {
 		return new ModelAndView("/schedule/index");
 	}

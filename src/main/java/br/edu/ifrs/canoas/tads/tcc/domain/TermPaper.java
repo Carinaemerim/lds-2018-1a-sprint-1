b8fdbae6fa1ac7dd594c00cbc6ef7685c862e2ec
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2018-03-21 16:08:53 -0300

    feat(board): added evaluation board JPA mapping

--- src/main/java/br/edu/ifrs/canoas/tads/tcc/domain/TermPaper.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/domain/TermPaper.java
@@ -1,17 +1,10 @@
 package br.edu.ifrs.canoas.tads.tcc.domain;
 
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
-import javax.validation.constraints.NotBlank;
-
 import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 @Data
@@ -28,8 +21,6 @@ public class TermPaper {
 	private User author;
     @ManyToOne
     private User advisor;
-    @OneToOne
-    private File document;
 
     /* Gerar a partir do abstract da monografia*/
     @Transient

639f2b545b66bf4d3aa0672317ad0c856bbf5b6c
Cassiano <cassianodoneda@gmail.com>
2018-03-24 01:10:19 -0300

    feat(evaluation, termpaper): termPaper evaluation confirmation message. Signed-off-by: Cassiano <cassianodoneda@gmail.com>

--- src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/EvaluationController.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/EvaluationController.java
@@ -11,12 +11,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -31,8 +32,8 @@ public class EvaluationController {
     private final DocumentService documentService;
     private final TermPaperService termPaperService;
 
-    @GetMapping("/list")
-    public ModelAndView greetings(@AuthenticationPrincipal UserImpl activeUser) {
+    @GetMapping("/")
+    public ModelAndView home(@AuthenticationPrincipal UserImpl activeUser) {
         ModelAndView mav = new ModelAndView("/evaluation/list");
         Iterable<TermPaper> termPapers = evaluationService.getTermPaperEvaluation(activeUser.getUser());
         mav.addObject("termPapers", termPapers);
@@ -71,6 +72,24 @@ public class EvaluationController {
         }
         mav.addObject("grade", grade);
         //model.addAttribute("action", "record");
+        return mav;
+    }
+
+    @PostMapping(path = "/termpaper/submit")
+    public ModelAndView saveTermPaperDraft(@AuthenticationPrincipal UserImpl activeUser, @Valid TermPaper termPaper,
+                                       BindingResult bindingResult, RedirectAttributes redirectAttr) {
+
+        ModelAndView mav = new ModelAndView("redirect:/evaluation/");
+
+        return mav;
+    }
+
+    @PostMapping(path = "/termpaper/submit", params = "action=evaluation")
+    public ModelAndView submitTermPaperForEvaluation(@AuthenticationPrincipal UserImpl activeUser,
+                                                 @Valid TermPaper termPaper, BindingResult bindingResult, RedirectAttributes redirectAttr) {
+
+        ModelAndView mav = new ModelAndView("redirect:/evaluation/");
+
         return mav;
     }
 

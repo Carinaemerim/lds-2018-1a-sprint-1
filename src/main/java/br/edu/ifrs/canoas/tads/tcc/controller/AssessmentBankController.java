991f35f3530052a834fb2da6559f390a4b1d05e7
KarenRamires <karenramires@gmail.com>
2018-03-29 00:39:50 -0300

    feat (List) listprofessors

--- src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/AssessmentBankController.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/controller/AssessmentBankController.java
@@ -5,12 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.ifrs.canoas.tads.tcc.service.UserService;
+import lombok.Data;
+
 @RequestMapping("/assessment-bank")
 @Controller
+@Data
 public class AssessmentBankController {
+	private final UserService userService;
 
 	@GetMapping("/list")
 	public ModelAndView assessmentBank() {		
 		return new ModelAndView("/assessment-bank/list");
 	}
+	
+	@GetMapping("/listprofessors")
+	public ModelAndView selectassessmentBank() {		
+		ModelAndView mav = new ModelAndView("/assessment-bank/listprofessors");
+		mav.addObject("professors", userService.getAdvisors());
+		return mav;
+	}	
+	
+	
+	
 }
--- src/main/java/br/edu/ifrs/canoas/tads/tcc/domain/Professor.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/domain/Professor.java
@@ -3,7 +3,10 @@ package br.edu.ifrs.canoas.tads.tcc.domain;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 import java.util.List;
 
 /**
@@ -16,6 +19,8 @@ public class Professor extends User{
     private String lattes;
     private String experience;
     private String skill;
+    @OneToMany(mappedBy = "advisor", fetch = FetchType.EAGER)
+    private List<TermPaper> termPapers;
     @ManyToMany
     private List<EvaluationBoard> board;
 

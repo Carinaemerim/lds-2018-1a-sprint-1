15454cbb3ede9efe4af88afd74233efd0a500602
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2018-03-30 10:56:04 -0300

    fix(upload): fixed invalid override of multipart file

--- src/main/java/br/edu/ifrs/canoas/tads/tcc/config/MVCConfig.java
+++ src/main/java/br/edu/ifrs/canoas/tads/tcc/config/MVCConfig.java
@@ -1,11 +1,8 @@
 package br.edu.ifrs.canoas.tads.tcc.config;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -38,13 +35,6 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
 
-	@Bean(name = "multipartResolver")
-	public CommonsMultipartResolver getCommonsMultipartResolver() {
-	    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-	    multipartResolver.setMaxUploadSize(20971520);   // 20MB
-	    multipartResolver.setMaxInMemorySize(1048576);  // 1MB
-	    return multipartResolver;
-	}
 
 
 }

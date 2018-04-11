f845cfc5ed3943c09e4a98a1f6963fcd8f5b2f73
Cassiano <cassianodoneda@gmail.com>
2018-04-11 19:06:38 -0300

    feat(evaluationTest): last professor evaluate TCC Signed-off-by: Cassiano <cassianodoneda@gmail.com>

--- src/test/java/br/edu/ifrs/canoas/tads/tcc/web/page/EvaluationPage.java
+++ src/test/java/br/edu/ifrs/canoas/tads/tcc/web/page/EvaluationPage.java
@@ -6,9 +6,12 @@ import org.fluentlenium.core.annotation.PageUrl;
 import org.fluentlenium.core.domain.FluentWebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.withText;
 
 @PageUrl("http://localhost:{port}/evaluation/")
 @Data
@@ -18,22 +21,81 @@ public class EvaluationPage extends FluentPage {
     private FluentWebElement buttonTheme;
 
     @FindBy(css = "#termPaper-4")
-    private FluentWebElement buttonTermPaperAvailableForEvaluation;
+    private FluentWebElement buttonTermPaperAvailableForEvaluationLastProfessor;
+
+    @FindBy(css = "#termPaper-0")
+    private FluentWebElement buttonTermPaperAvailableForEvaluationNotLastProfessor;
+
+
+    @FindBy(css = "#grade-4")
+    private FluentWebElement gradeFinalLastProfessor;
+
+    @FindBy(css = "#grade-status-4")
+    private FluentWebElement gradeStatusLastProfessor;
+
+    @FindBy(css = "#grade-0")
+    private FluentWebElement gradeFinalNotLastProfessor;
+
+    @FindBy(css = "#grade-status-0")
+    private FluentWebElement gradeStatusNotLastProfessor;
 
     @FindBy(css = "#termPaper-detail")
     private FluentWebElement termPaperDetail;
-   /* @FindBy(css = "#search-button")
-    private FluentWebElement searchButton;*/
 
+    @FindBy(css = "#form-termpaper-advisor")
+    private FluentWebElement formEvaluationTermPaper;
+
+    @FindBy(css = "#buttonSubmitEvaluation")
+    private FluentWebElement buttonSubmitEvaluation;
+
+    @FindBy(css = "#buttonSubmitDraftEvaluation")
+    private FluentWebElement buttonDraftSubmitEvaluation;
+
+
+    @FindBy(css = "#gradeEvaluationSubmitConfirmationModal")
+    private FluentWebElement gradeEvaluationSubmitConfirmationModal;
+
+    @FindBy(css = "#yesSubmit")
+    private FluentWebElement buttonYesSubmitModal;
+
+    @FindBy(css = "#table-list-evaluate_length")
+    private FluentWebElement tableListEvaluation;
+
+    @FindBy(css = "#appraiser-name")
+    private FluentWebElement appraiserName;
+
+    @FindBy(css = "#considerations")
+    private FluentWebElement considerations;
+
+    @FindBy(css = "#final-grade-by-professor")
+    private FluentWebElement finalGradeByProfessor;
+
+    @FindBy(css = "# date-and-hour")
+    private FluentWebElement dateAndHour;
+
+
+
+
+  /*  @FindBy(xpath = "//*[text() = Rascunho salvo com sucesso!")
+    private FluentWebElement messageDraftSaved;*/
 
     public void isAt() {
         assertThat(window().title()).isEqualTo("Avaliação de Trabalhos");
     }
 
     public void isAtTermPaperEvaluation() {
-
-        buttonTermPaperAvailableForEvaluation.click();
         assertThat(window().title()).isEqualTo("Avaliação de Monografia");
+    }
+
+    public EvaluationPage selectTermPaperForEvaluationLastProfessor() {
+        buttonTermPaperAvailableForEvaluationLastProfessor.click();
+        return this;
+
+    }
+    public EvaluationPage selectTermPaperForEvaluationNotLastProfessor() {
+        buttonTermPaperAvailableForEvaluationNotLastProfessor.click();
+        return this;
+
     }
 
     public EvaluationPage awaitUntilTermPaperAppear() {
@@ -41,12 +103,60 @@ public class EvaluationPage extends FluentPage {
         return this;
     }
 
-    /*
-    public EvaluationPage fillAndSubmitForm(String... paramsOrdered) {
-        $("input").fill().with(paramsOrdered);
-        buttonTheme.click();
+    public EvaluationPage awaitUntilFormEvaluationTermPaperAppear() {
+        await().atMost(5, TimeUnit.SECONDS).until(formEvaluationTermPaper).present();
         return this;
     }
+    public EvaluationPage awaitUntilTableListEvaluateAppear() {
+        await().atMost(5, TimeUnit.SECONDS).until(tableListEvaluation).present();
+        return this;
+    }
+
+
+    public EvaluationPage awaitUntilAppraiserNameAppear() {
+        await().atMost(5, TimeUnit.SECONDS).until(appraiserName).text().startsWith("orientador");
+        return this;
+    }
+
+
+    public EvaluationPage awaitFiveSeconds() {
+        await().atMost(5, TimeUnit.SECONDS);
+        return this;
+    }
+
+    public EvaluationPage fillAndSubmitForm(String... paramsOrdered) {
+        $("input").fill().with(paramsOrdered);
+        buttonSubmitEvaluation.click();
+        return this;
+    }
+
+    public EvaluationPage fillAndSubmitDraftForm(String... paramsOrdered) {
+        $("input").fill().with(paramsOrdered);
+        buttonDraftSubmitEvaluation.click();
+        return this;
+    }
+
+    public EvaluationPage awaitUntilConfirmationModal() {
+        await().atMost(5, TimeUnit.SECONDS).until(gradeEvaluationSubmitConfirmationModal).present();
+        return this;
+    }
+
+    public String getFileAbsolutePath(){
+        File file = Paths.get(".", "src","main","resources","static","photos","face.jpg").normalize().toFile();
+
+        return file.getAbsolutePath();
+    }
+
+    public EvaluationPage selectConfirmSubmit() {
+        buttonYesSubmitModal.click();
+        return this;
+
+    }
+
+    public void fillTextAreaForm(String... paramsOrdered) {
+        $("textarea").fill().with(paramsOrdered);
+    }
+    /*
 
 
     public EvaluationPage openResult() {
--- src/test/java/br/edu/ifrs/canoas/tads/tcc/web/test/EvaluationTest.java
+++ src/test/java/br/edu/ifrs/canoas/tads/tcc/web/test/EvaluationTest.java
@@ -6,6 +6,13 @@ import org.fluentlenium.core.annotation.Page;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.validation.constraints.AssertFalse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.fluentlenium.assertj.FluentLeniumAssertions.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class EvaluationTest extends MyFluentTest {
 
@@ -18,26 +25,85 @@ public class EvaluationTest extends MyFluentTest {
     }
 
     @Test
-    public void checkListForBoot() {
+    public void lastProfessorEvaluateTermPaper() {
+
         //Given
         evaluationPage.go(port);
         evaluationPage.isAt();
+        evaluationPage.selectTermPaperForEvaluationLastProfessor()
+                .awaitUntilFormEvaluationTermPaperAppear();
         evaluationPage.isAtTermPaperEvaluation();
-        evaluationPage.awaitUntilTermPaperAppear();
 
-       /* //When
-        catalogPage.fillAndSubmitForm("boot")
-                .awaitUntilResultsAppear();
-        //and
-        catalogPage.openResult();
-        //and
-        catalogPage.downloadFile();
-
+        //When
+        evaluationPage.fillTextAreaForm("Aprovado, revise o documento anexado");
+        evaluationPage.fillAndSubmitForm("","9", "6", "7", "7", "5", "8","7", "8" , "9" , "10", "8", "7",evaluationPage.getFileAbsolutePath())
+                .awaitUntilConfirmationModal();
+        evaluationPage.selectConfirmSubmit()
+                .awaitUntilTableListEvaluateAppear();
 
         //Then
-        assertThat(window().title()).isEqualTo("Catálogo");
-        assertThat(catalogPage.getFirstResultDetails().displayed()).isTrue();
-        assertThat(catalogPage.getFirstResultDetails().text()).containsIgnoringCase("Assim como o título, o resumo");*/
+        assertThat(window().title()).isEqualTo("Avaliação de Trabalhos");
+        assertThat(evaluationPage.getGradeFinalLastProfessor().text()).containsIgnoringCase("7,6");
+        assertThat(evaluationPage.getGradeStatusLastProfessor().text()).containsIgnoringCase("APROVADO");
+
+    }
+
+    @Test
+    public void notLastProfessorEvaluateTermPaper() {
+
+        //Given
+        evaluationPage.go(port);
+        evaluationPage.isAt();
+        evaluationPage.selectTermPaperForEvaluationNotLastProfessor()
+                .awaitUntilFormEvaluationTermPaperAppear();
+        evaluationPage.isAtTermPaperEvaluation();
+
+        //When
+        evaluationPage.fillTextAreaForm("Aprovado, revise o documento anexado");
+        evaluationPage.fillAndSubmitForm("","9", "6", "7", "7", "5", "8","7", "8" , "9" , "10", "8", "7",evaluationPage.getFileAbsolutePath())
+                .awaitUntilConfirmationModal();
+        evaluationPage.selectConfirmSubmit()
+                .awaitUntilTableListEvaluateAppear();
+
+        //Then
+        assertThat(window().title()).isEqualTo("Avaliação de Trabalhos");
+        assertThat(evaluationPage.getGradeFinalNotLastProfessor().text()).containsIgnoringCase("");
+        assertThat(evaluationPage.getGradeStatusNotLastProfessor().text()).containsIgnoringCase("EM PROGRESSO");
+        // todo add mensagem com sucesso
+
+    }
+
+    @Test
+    public void saveDraftEvaluationTermPaper() {
+
+        //Given
+        evaluationPage.go(port);
+        evaluationPage.isAt();
+        evaluationPage.selectTermPaperForEvaluationLastProfessor()
+                .awaitUntilFormEvaluationTermPaperAppear();
+        evaluationPage.isAtTermPaperEvaluation();
+
+        //When
+        evaluationPage.fillTextAreaForm("Aprovado, revise o documento anexado");
+        evaluationPage.fillAndSubmitDraftForm("","9", "6", "7", "7", "5", "8","7", "8" , "9" , "10", "8", "7",evaluationPage.getFileAbsolutePath())
+        .awaitUntilAppraiserNameAppear();
+
+        //Then
+        assertThat(window().title()).isEqualTo("Avaliação de Monografia");
+     /*   assertThat($(".div", withClass("callout callout-success lead")));
+        //assertThat($(".textarea")).hasT("Aprovado, revise o documento anexado");
+        assertThat(el("#considerations").text().equals("xAprovado, revise o documento anexado"));
+        assertThat(!(el("#considerations").displayed()));
+        //assertThat(el("#considerations").text().);*/
+        assertThat(el("#date-and-hour").text().contains("??"));
+
+
+        assertThat(evaluationPage.getDateAndHour());
+        //ok
+        assertThat(evaluationPage.getFinalGradeByProfessor().text()).containsIgnoringCase("7,6");
+
+
+
     }
 
 }
